c17b896eeb9e0feb62874fb5d10a52b042845c70
Jonathan Smith <jonathan@peoplesized.com>
2017-10-11 18:00:22 +0100

    plain place to start

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      '@': resolve('src'),
+      'components': resolve('src/components'),
     }
   },
   module: {
---
+++ src/components/thing.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>hello thing</div>
+</template>
+
+<script>
+  // import { mapState, mapActions, mapMutations } from 'vuex'
+
+  export default {
+    name: 'thing',
+    mounted () {
+    },
+    data () {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
