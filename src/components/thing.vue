454f98844ef9ddf8f40628cee2f109e94d2844b3
Jonathan Smith <jonathan@peoplesized.com>
2017-10-11 18:11:52 +0100

    vue-material

--- src/components/thing.vue
+++ src/components/thing.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>hello thing</div>
+  <md-toolbar>
+    <md-button class="md-icon-button">
+      <md-icon>menu</md-icon>
+    </md-button>
+
+    <h2 class="md-title" style="flex: 1">Thing</h2>
+
+  </md-toolbar>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 import App from './App'
 import router from './router'
 import store from './store'
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
