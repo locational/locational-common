2dd0207a4a3c0f11bc35e47a6b7d3d67598a8158
Jonathan Smith <jonathan@peoplesized.com>
2017-10-12 11:50:32 +0100

    ready to assemble

---
+++ src/assemble_config.js
@@ -0,0 +1,9 @@
+const def = [['view_things', 'data_things'], ['page_things', 'view_things']] // 'view_things' have 'data_things'
+
+export const assemble_config = (config) => {
+  // expand relations
+  // copy across `config`
+  // nest relations
+  return config
+}
+
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 import VueGoodTable from 'vue-good-table'
+
 import App from './App'
 import router from './router'
 import store from './store'
+import {assemble_config} from 'assemble_config'
 
 Vue.use(VueMaterial)
 Vue.use(VueGoodTable)
@@ -29,7 +31,10 @@ const launch = async () => {
   // Simulate retrieving/inserting config from somewhere
   const config = await fetch('/static/configs/foci.json').then(res => res.json())
   // This commit works, but doesn't display in vue-devtools
-  store.commit('set_config', config)
+
+  const assembled_config = assemble_config(config)
+
+  store.commit('set_config', assembled_config)
 }
 
 launch().catch(console.error)
