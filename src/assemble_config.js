26eed1afe8e08a0cdea58a40f8da6b9cbb9d2a71
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-10-12 13:48:01 +0100

    Testing and refac

--- src/assemble_config.js
+++ src/assemble_config.js
@@ -1,5 +1,3 @@
-import cloneDeep from 'lodash/cloneDeep'
-
 const def = [['view_things', 'data_things'], ['page_things', 'view_things']] // 'view_things' have 'data_things'
 
 export const assemble_config = (config) => {
@@ -15,17 +13,15 @@ export const assemble_config = (config) => {
     // find the actual_objects from property_with_ids_from_src_object
     for (const object_from_config_to_find_id_for of config[src_object_name]) {
       // object_from_config_to_find_id_for is a view_thing
-      const ids_to_find = cloneDeep(object_from_config_to_find_id_for[property_with_ids_from_src_object])
-      object_from_config_to_find_id_for[property_with_ids_from_src_object] = []
+      const ids_to_find = object_from_config_to_find_id_for[property_with_ids_from_src_object]
 
       for (const id of ids_to_find) {
         const found = config[property_with_ids_from_src_object].find(o => o.id === id)
-        console.log('found', found)
-        object_from_config_to_find_id_for[property_with_ids_from_src_object].push(found)
+        const found_index = config[property_with_ids_from_src_object].findIndex(o => o.id === id)
+        object_from_config_to_find_id_for[property_with_ids_from_src_object][found_index] = found
       }
     }
   }
   delete output[def[0][0]]
-  console.log('output', output)
   return output
 }
