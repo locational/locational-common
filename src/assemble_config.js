ad2c25edd49724b8fde623e98d6b60bf1518c133
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-10-12 15:31:56 +0100

    First version of schemas

--- src/assemble_config.js
+++ src/assemble_config.js
@@ -13,11 +13,11 @@ export const assemble_config = (config) => {
     // find the actual_objects from property_with_ids_from_src_object
     for (const object_from_config_to_find_id_for of config[src_object_name]) {
       // object_from_config_to_find_id_for is a view_thing
-      const ids_to_find = object_from_config_to_find_id_for[property_with_ids_from_src_object]
+      const objects_to_find = object_from_config_to_find_id_for[property_with_ids_from_src_object]
 
-      for (const id of ids_to_find) {
-        const found = config[property_with_ids_from_src_object].find(o => o.id === id)
-        const found_index = config[property_with_ids_from_src_object].findIndex(o => o.id === id)
+      for (const object of objects_to_find) {
+        const found = config[property_with_ids_from_src_object].find(o => o.id === object.id)
+        const found_index = config[property_with_ids_from_src_object].findIndex(o => o.id === object.id)
         object_from_config_to_find_id_for[property_with_ids_from_src_object][found_index] = found
       }
     }
