86d95807d3caa2a88ed1a5ae809a5d618e676503
Jonathan Smith <jonathan@peoplesized.com>
2017-10-20 10:23:33 +0200

    works

--- src/config/assemble_config/index.ts
+++ src/config/assemble_config/index.ts
@@ -1,46 +1,45 @@
-import {ConfigThing} from "./index.d"
-import {pluck} from "../../util/pluck"
+import {cloneDeep} from "lodash";
 
-export function assemble(source: any): ConfigThing {
-  let destination: ConfigThing = pluck(source, 'meta') // Start with `meta` copied
-  Object.freeze(source) // Does this do what I think it does?
+function replace_id_with_object(target: any, objects_array: any[]) {
+  const found = objects_array.find(o => o.id === target.id)
+  return {
+    ...target, // keep all properties on original target
+    ...found
+  }
+}
+const steps = [
+  {parent_key: 'view_things', child_key: 'data_things'},
+  {parent_key: 'page_things', child_key: 'view_things'}
+]
 
-  const step = [{parent_key: 'view_things', child_key: 'data_things'}, {parent_key: 'page_things', child_key: 'view_things'}]
-  
-  // Decorate onto fattened_array
-  let fattened_array = [] // To hold the ids-replaced-with-objects
-  step.forEach(step => {
-    const parent_key = step.parent_key
-    const child_key = step.child_key
-    // get the ids-only version of the parent from the source
-    const parent_objects: any[] = source[step.parent_key]
-    // create the objects-version
-    const objects_version = iterate_a_level({source, parent_objects, child_key})
-    // write the objects-version to the fattened_array
-    fattened_array[parent_key] = objects_version
+function replace_all_ids_with_objects(source: any) {
+  let replaced = {}
+
+  steps.forEach(({parent_key, child_key}) => {
+    const parent_objects: any[] = source[parent_key]
+    const source_child_objects = replaced[child_key] || source[child_key]
+    const things = parent_objects.map(parent => {
+      const children = parent[child_key]
+      const replaced_children = children.map(child => {
+        return replace_id_with_object(child, source_child_objects)
+      })
+      return {
+        ...parent,
+        [child_key]: replaced_children
+      }
+    })
+    replaced[parent_key] = things
   })
-  
-  console.log('fattened_array', fattened_array)
-  debugger
 
-  return destination
+  return replaced
 }
 
-function iterate_a_level({source, parent_objects, child_key}): any[] {
-  return parent_objects.map(level => {
-    const ids_array = parent_objects
-    const objects_array = source[child_key]
-    const result = replace_ids_with_objects(ids_array, objects_array)
+export function assemble(source: any) {
+  const cloned_source = cloneDeep(source)
+  const replaced = replace_all_ids_with_objects(cloned_source)
 
-  })
+  return {
+    meta: source.meta,
+    page_things: replaced['page_things']
+  }
 }
-
-function replace_ids_with_objects(ids_array: number[], objects_array: any[]): any[] {
-  return ids_array.map(id => {
-    const found = objects_array.find(o => o.id === id)
-  }).filter(i => i)
-}
-
-
-// decorate in place
-// assemble according to recipe
